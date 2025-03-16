c2d962a0e6abb010d5e9241e4ba585ad676404d8
Vasanthan.Arulsivanathan <V.Arulsivanathan@outlook.de>
2025-03-16 22:11:53 +0100

    V7 first Commit

---
+++ src/main/java/com/example/gptapibackend/GptApiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gptapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptApiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GptApiBackendApplication.class, args);
+    }
+
+}
